cabd5356a827545b5b2328e844f01b9edf6a4445
Neo Brahma Studio <neo.brahma.studio@gmail.com>
2023-12-11 10:03:32 +0100

    Sudoku: optimize FindOnePossibilityByCellUseCase

--- app/src/main/java/com/neobrahma/puzzlegame/domain/sudoku/usecase/resolve/FindOnePossibilityByCellUseCase.kt
+++ app/src/main/java/com/neobrahma/puzzlegame/domain/sudoku/usecase/resolve/FindOnePossibilityByCellUseCase.kt
@@ -6,19 +6,27 @@ class FindOnePossibilityByCellUseCase(
     private val nextAlgo: ResolverAlgo? = null
 ) : ResolverAlgo {
     override fun invoke(sudokuGrid: SudokuGrid): ResolverAlgoResult {
-        sudokuGrid.cells.forEachIndexed { indexCell, sudokuCell ->
-            val possibilities = mutableListOf<Int>()
-            sudokuCell.possibleValue.forEachIndexed { index, value ->
-                if(value != 0){
-                    possibilities.add(value*(index+1))
+         sudokuGrid.cells.forEachIndexed loop@{ indexCell, sudokuCell ->
+            if(sudokuCell.value == 0) {
+
+                var result = 0
+                sudokuCell.possibleValue.forEachIndexed { index, value ->
+                    if(value != 0) {
+                        if (result == 0) {
+                            result = value * (index + 1)
+                        } else {
+                            return@loop
+                        }
+                    }
                 }
-            }
-            if (possibilities.isNotEmpty() && possibilities.size == 1) {
-                return ResolverAlgoResult.FindOnePossibility(
-                    text = "find one possiblity",
-                    index = indexCell,
-                    value = possibilities[0]
-                )
+
+                if(result !=0) {
+                    return ResolverAlgoResult.FindOnePossibility(
+                        text = "find one possiblity",
+                        index = indexCell,
+                        value = result
+                    )
+                }
             }
         }
         return nextAlgo?.invoke(sudokuGrid) ?: ResolverAlgoResult.Nothing
